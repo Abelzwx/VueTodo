54c448c067af30d7ebb616114aff9acc38b29dad
Abel-zwx <abel-zwx@Abel-zwxdeMacBook-Pro.local>
2018-07-26 22:10:20 +0800

    update

--- src/todo/todo.vue
+++ src/todo/todo.vue
@@ -3,7 +3,7 @@
         <input type="text"
             class="add-input"
             autofocus="autofocus"
-            placeholder="请输入任务"
+            placeholder="请输入任务内容"
             @keyup.enter="addTodo"
         >
 
